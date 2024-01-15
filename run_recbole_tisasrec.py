bf8951d1eb3882c7158180d6ddb54a8928490d80
zyw <214926@seu.edu.cn>
2024-01-15 11:44:25 +0800

    the config for tisasrec

--- run_recbole_tisasrec.py
+++ run_recbole_tisasrec.py
@@ -5,7 +5,7 @@ from recbole_trm.quick_start import run_recbole_trm
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', '-m', type=str, default='BPR', help='name of models')
+    parser.add_argument('--model', '-m', type=str, default='TiSASRec', help='name of models')
     parser.add_argument('--dataset', '-d', type=str, default='ml-100k', help='name of datasets')
     # parser.add_argument('--config_files', type=str, default=None, help='config files')
     parser.add_argument('--config_files', type=str, default='config/TiSASRec.yaml', help='config files')
